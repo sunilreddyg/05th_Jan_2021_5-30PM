9abab46e9d7ab61285cea0e3a37f69e504a2e8ae
Administrator <Administrator@192.168.29.247>
2021-01-19 19:30:31 +0530

    Object identification

--- Project_185/src/webdriver/selenium/WebDriver_InterFace.java
+++ Project_185/src/webdriver/selenium/WebDriver_InterFace.java
@@ -3,9 +3,11 @@ package webdriver.selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class WebDriver_InterFace {
+public class WebDriver_InterFace 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		
 		System.setProperty("webdriver.chrome.driver","Drivers\\chromedriver.exe");
