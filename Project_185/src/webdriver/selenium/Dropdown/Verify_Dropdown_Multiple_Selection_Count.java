d92934538fa085c625e87c71f8ddf084086f352d
Administrator <Administrator@AA-PC>
2021-01-25 19:47:17 +0530

    Multile Selection Commands

--- Project_185/src/webdriver/selenium/Dropdown/Verify_Dropdown_Multiple_Selection_Count.java
+++ Project_185/src/webdriver/selenium/Dropdown/Verify_Dropdown_Multiple_Selection_Count.java
@@ -6,16 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class Select_Multiple_Options_At_Dropdown {
+public class Verify_Dropdown_Multiple_Selection_Count
+{
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args)  throws Exception
+	{
+		
 		
 		/*
-		 * Faq:--> How to select multiple option under dropdown.
-		 * Ans:--> Repeat Selection command multiple times
-		 * 		[Ther is no separate command for multiple slelection]
+		 * Faq:--> How to verify dropdown is selected more than one option.
 		 */
 		
+
 		//setting runtime environment variable
 		System.setProperty("webdriver.chrome.driver", "Drivers\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();     //Launching browser method
@@ -44,17 +46,39 @@ public class Select_Multiple_Options_At_Dropdown {
 	    	State_Dropdown.selectByIndex(3);
 	    	State_Dropdown.selectByIndex(5);
 	    	State_Dropdown.selectByIndex(7);
+	    	State_Dropdown.selectByIndex(9);
 	    	
-	    	System.out.println(State_Dropdown.getAllSelectedOptions().size());
+	    	//Get Selected Option Count
+	    	int Selection_Count=State_Dropdown.getAllSelectedOptions().size();
+		    	//Condition to verify Dropdown Selecton count
+		    	if(Selection_Count > 1)
+		    	{
+		    		System.out.println("PreCondition Pass, Dropdown allowed user to select multiple option count");
+		    			//Nested if condition to check maximum selection limit..
+		    			if(Selection_Count <4)
+		    			{
+		    				System.out.println("Testpass, Dropdown is not exceeding dropdown selection");
+		    			}
+		    			else
+		    			{
+		    				System.out.println("Testfail,Dropdown is exceeding selection limit, Expected is ==>3");
+		    			}
+		    		
+		    		
+		    	}
+		    	else
+		    	{
+		    		System.out.println("PreCondition Failed,Dropdown failed to select multiple options");
+		    	}
+	    
+	  
 	    }
 	    else
 	    {
 	    	System.out.println("Dropdown is not a multiple selection type");
 	    }
-	    
-	    
-	    
-	    
+			    
+			    
 
 	}
 
